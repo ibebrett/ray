f89307f1910b44c00c7185e75713977ff6b90721
Brett Jurman <i.be.brett@gmail.com>
2015-03-14 15:32:29 -0400

    added juicy pixels dep, and added blue screen proof of concept

---
+++ Main.hs
@@ -0,0 +1,19 @@
+import Data.Word
+import Codec.Picture
+import Codec.Picture.Types
+
+main :: IO ()
+
+
+black :: Word8
+black = 0
+
+white :: Word8
+white = 255
+
+imageCreator :: String -> IO ()
+imageCreator path = writePng path $ generateImage genPixel 300 200
+    where genPixel x y = PixelRGB8 black black white
+
+
+main = imageCreator "test.png"
