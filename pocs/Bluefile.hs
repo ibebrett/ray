b68a473325c79cc27231f76723ef3fc0b6baa332
Brett Jurman <i.be.brett@gmail.com>
2015-03-15 11:57:04 -0400

    added circle rendering to proof of concept, moved src to src

--- pocs/Bluefile.hs
+++ pocs/Bluefile.hs
@@ -4,14 +4,21 @@ import Codec.Picture.Types
 
 main :: IO ()
 
-black :: Word8
-black = 0
+off :: Word8
+off = 0
 
-white :: Word8
-white = 255
+on :: Word8
+on = 255
+
+blue = PixelRGB8 off off on
+white = PixelRGB8 on on on
+
+circleFunc :: Double -> Double -> Double -> PixelRGB8
+circleFunc r x y = if dist < r then blue else white
+    where dist = sqrt ((x - 150.0)**2 + (y - 100.0)**2)
 
 imageCreator :: String -> IO ()
 imageCreator path = writePng path $ generateImage genPixel 300 200
-    where genPixel x y = PixelRGB8 black black white
+    where genPixel x y = circleFunc 30.0 (fromIntegral x) (fromIntegral y)
 
 main = imageCreator "test.png"
